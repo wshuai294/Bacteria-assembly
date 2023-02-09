e7fd100e8633d7d8705478b0b4f7aaac4c1209ed
wshuai294 <wshuai294@gmail.com>
2023-02-09 20:38:06 +0800

    freebayes-parallele

--- scripts/polish_contigs.py
+++ scripts/polish_contigs.py
@@ -1,11 +1,14 @@
 import sys, os, re
 from Bio import SeqIO
+import pysam
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
 
 min_sv_len = 50
+min_variant_q = 20
 
 def read_delly_vcf(vcffile):
-    my_breakpoints = []
-    remove_repeat = {}
+    my_breakpoints = {}
     if not os.path.isfile(vcffile):
         print ("no sv vcf.")
         return 0
@@ -21,18 +24,20 @@ def read_delly_vcf(vcffile):
         chrom = array[0]
         pos = int(array[1])
 
+        if chrom not in my_breakpoints:
+            my_breakpoints[chrom] = []
+
         if not re.search("SVTYPE=BND", line):
             sv_len = abs(int(end_pos.group(1)) - pos)
             if sv_len < min_sv_len:
                 continue
-            if chrom + "_" + str(pos) not in remove_repeat:
-                my_breakpoints.append([chrom, pos])
-                remove_repeat[chrom + "_" + str(pos)] = 1
+            my_breakpoints[chrom].append(pos)
+
             if array[4] == "<DEL>" or array[4] == "<DUP>":
                 pos_end = pos + sv_len
-            if chrom + "_" + str(pos) not in remove_repeat:
-                my_breakpoints.append([chrom, pos_end])
-                remove_repeat[chrom + "_" + str(pos_end)] = 1
+
+            my_breakpoints[chrom].append(pos_end)
+
         else:
             fir = re.search("\[(.*?)\[", array[4])
             sec = re.search("](.*?)]", array[4])
@@ -45,15 +50,19 @@ def read_delly_vcf(vcffile):
             pos2 = int (locus_info[1])
             if chrom2 == chrom and abs(pos2 - pos) < min_sv_len:
                 continue
-            if chrom + "_" + str(pos) not in remove_repeat:
-                my_breakpoints.append([chrom, pos])
-                remove_repeat[chrom + "_" + str(pos)] = 1
-            if chrom2 + "_" + str(pos2) not in remove_repeat:
-                my_breakpoints.append([chrom2, pos2])
-                remove_repeat[chrom2 + "_" + str(pos2)] = 1
-    return sorted(my_breakpoints)
 
-def split_fasta(input_fasta, output_fasta, breakpoint_list):
+            my_breakpoints[chrom].append(pos)
+            if chrom2 not in my_breakpoints:
+                my_breakpoints[chrom2] = []
+            my_breakpoints[chrom2].append(pos2)
+    
+    for chrom in my_breakpoints:
+        my_breakpoints[chrom] = list(set(my_breakpoints[chrom]))
+        my_breakpoints[chrom].sort()
+        # print (chrom, my_breakpoints[chrom])
+    return my_breakpoints
+
+def split_fasta(input_fasta, output_fasta, my_breakpoints):
     # Load the FASTA file
     fasta_file = SeqIO.parse(input_fasta, "fasta")
     handle = open(output_fasta, "w")
@@ -61,23 +70,134 @@ def split_fasta(input_fasta, output_fasta, breakpoint_list):
     for record in fasta_file:
         start = 0
         end = len(record)
+        if record.id in my_breakpoints:
+            breakpoint_list = my_breakpoints[record.id]
+        else:
+            breakpoint_list = []
         for breakpoint in breakpoint_list:
-            if breakpoint[0] == record.id:
-                if abs(start - breakpoint[1]) > min_sv_len:
-                    new_record = record[start:breakpoint[1]]
-                    new_record.id = f"{record.id}_{start}_{breakpoint[1]}"
-                    new_record.description = ""
-                    SeqIO.write(new_record, handle, "fasta")
-                    start = breakpoint[1]
+            if abs(start - breakpoint) > min_sv_len:
+                new_record = record[start:breakpoint]
+                new_record.id = f"{record.id}_{start}_{breakpoint}"
+                new_record.description = ""
+                SeqIO.write(new_record, handle, "fasta")
+                start = breakpoint
         if abs(start - end) > min_sv_len:
             new_record = record[start:end]
             new_record.id = f"{record.id}_{start}_{end}"
             new_record.description = ""
             SeqIO.write(new_record, handle, "fasta")
+
+def read_mismatch(vcf, method):
+
+    vcf = pysam.VariantFile(vcf)
+
+    for record in vcf.fetch():
+        if record.qual < min_variant_q:
+            continue
+        ref = record.ref
+        alt = record.alts[0]
+
+        if method == "svaba" and abs(len(ref) - len(alt)) < min_sv_len:
+            continue
+        if method == "freebayes" and abs(len(ref) - len(alt)) >= min_sv_len:
+            continue
+        if record.chrom not in my_mismatches:
+            my_mismatches[record.chrom] = []
+        my_mismatches[record.chrom].append([record.chrom, record.pos, ref, alt])
     
-vcffile = sys.argv[1]
-input_fasta = sys.argv[2]
-output_fasta = sys.argv[3]
-my_breakpoints = read_delly_vcf(vcffile)
-# print (my_breakpoints)
-split_fasta(input_fasta, output_fasta, my_breakpoints)
+    vcf.close()
+
+def get_consensus():
+    fasta_file = SeqIO.parse(input_fasta, "fasta")
+    handle = open(middle_fasta, "w")
+
+    position_correction = {}
+
+    for record in fasta_file:
+        # Create a list to store the consensus sequence
+        consensus = []
+        position_correction[record.id] = {}
+        relative_pos = 0 
+
+        if record.id in my_mismatches:
+            variant_list = my_mismatches[record.id]
+            variant_list.sort(key = lambda x: x[1])
+            original_length = len(record.seq)
+            reference_seq = str(record.seq)
+            
+            # Initialize a start position and end position for each variant
+            start = 0
+            end = 0
+            for chrom, pos, ref, alt in variant_list:
+                # Update the end position to be the start of the current variant
+                end = pos - 1
+                
+                # Add the reference sequence between the start and end positions to the consensus
+                consensus.append(reference_seq[start:end])
+                
+                # Update the start position to be the end of the current variant
+                start = end + len(ref)
+                
+                # Add the alternate sequence to the consensus
+                consensus.append(alt)
+
+                relative_pos = relative_pos + (len(alt) - len(ref))
+                position_correction[record.id][pos] = relative_pos
+            
+            # Add the remaining reference sequence to the consensus
+            consensus.append(reference_seq[start:])
+            
+            # Join the consensus sequence into a single string
+            consensus_seq = "".join(consensus)
+            record.seq = Seq(consensus_seq)
+        
+        SeqIO.write(record, handle, "fasta")
+    
+    handle.close()
+    return position_correction
+
+def correct_sv_position(my_breakpoints, position_correction):
+    revised_breakpoints = {}
+    for chrom in my_breakpoints:
+        revised_breakpoints[chrom] = []
+
+        if chrom not in position_correction:
+            revised_breakpoints[chrom] = my_breakpoints[chrom]
+            continue
+
+        for original_pos in my_breakpoints[chrom]:
+            pos_diff = 0
+            for variant_locus in position_correction[chrom]:
+                if variant_locus > original_pos:
+                    break
+                if original_pos >= variant_locus:
+                    pos_diff = position_correction[chrom][variant_locus]
+            revised_pos = original_pos + pos_diff
+
+            revised_breakpoints[chrom].append(revised_pos)
+    return revised_breakpoints
+
+
+
+if __name__ == "__main__":
+
+    freebayes_vcf = sys.argv[1]
+    svaba_indel = sys.argv[2]
+    svaba_sv = sys.argv[3]
+
+    input_fasta = sys.argv[4]
+    middle_fasta = input_fasta + ".tmp.fasta"
+    output_fasta = sys.argv[5]
+
+
+    my_mismatches = {}
+    read_mismatch(freebayes_vcf, "freebayes")
+    read_mismatch(svaba_indel, "svaba")
+    my_breakpoints = read_delly_vcf(svaba_sv)
+    position_correction = get_consensus()
+    # print (my_breakpoints)
+    my_breakpoints = correct_sv_position(my_breakpoints, position_correction)
+    # print (my_breakpoints)
+
+
+    split_fasta(middle_fasta, output_fasta, my_breakpoints)
--- scripts/workflow3.sh
+++ scripts/workflow3.sh
@@ -1,14 +1,12 @@
 # requirments: bwa samtools spades(download binary) lumpyexpress blast freebayes vcftools mummer
 # python modules: pysam  pyyaml Biopython scikit-learn pyfaidx matplotlib pandas seaborn networkx
-# sudo apt -y install delly
-# gnuplot
 
-ref=$1
+
+ref_list=$1
 fq1=$2
 fq2=$3
 ID=$4
 outdir=$5
-# true=$6
 
 sample=$outdir/$ID
 ins_ref=$outdir/$ID.ins.fa
@@ -23,6 +21,12 @@ mkdir $outdir
 map_qual=20
 threads=15
 
+$dir/select_ref $fq1 $fq2 $ref_list 26 10 $threads $sample.match_rate.csv 30
+ref=$(awk -F',' 'BEGIN { max = 0 } 
+         { if($2>max) { max=$2; val=$1 } } 
+         END { print val }' $sample.match_rate.csv)
+echo "selected ref is $ref."
+
 # :<<!
 bwa index $ref
 samtools faidx $ref
@@ -31,33 +35,32 @@ bwa mem -t $threads -R "@RG\tID:id\tSM:sample\tLB:lib" $ref $fq1 $fq2 \
 samtools sort -o $sample.init.bam $sample.unsort.bam
 samtools index $sample.init.bam
 samtools depth $sample.init.bam >$sample.init.depth
-# delly call -g $ref -o $sample.delly.sv.bcf $sample.init.bam
-# bcftools view $sample.delly.sv.bcf >$sample.delly.sv.vcf
 svaba run -t $sample.init.bam -G $ref -a $sample -p $threads
 python $dir/extract_short_insertions.py $sample.svaba.indel.vcf $sample.short.INS.fasta $sample.short.INS.pos.txt
-# python3 $dir/ref_segment.py $ref $seg_ref $sample.delly.sv.vcf $sample.init.depth $sample.init.bam $sample.short.segs.txt $sample.short.INS.pos.txt
 python3 $dir/ref_segment.py $ref $seg_ref $sample.svaba.sv.vcf $sample.init.depth $sample.init.bam $sample.short.segs.txt $sample.short.INS.pos.txt
 cat $sample.short.INS.fasta >>$seg_ref
 
 end=$(date +%s)
 take=$(( end - start ))
-echo one Time taken to map reads is ${take} seconds. # >> ${sample}.log
+echo one Time taken to segmentation is ${take} seconds. # >> ${sample}.log
 
 
 python3 $dir/extract_unmap.py $sample.init.bam $sample.unmap.bam $map_qual $sample.short.segs.txt
 samtools fastq -1 $sample.unmapped.1.fq -2 $sample.unmapped.2.fq -s $sample.unmapped.s.fq -@ 8 $sample.unmap.bam
 gzip -f $sample.unmapped.*fq
-rm -r $outdir/ass
+if [ -d "$outdir/ass" ]; then
+  rm -r $outdir/ass
+fi
 echo "start spades..."
 spades.py --isolate -t $threads -1 $sample.unmapped.1.fq.gz -2 $sample.unmapped.2.fq.gz -s $sample.unmapped.s.fq.gz -o $outdir/ass >$sample.spades.log
 python $dir/filter_assemblies.py $outdir/ass/contigs.fasta $outdir/ass/contigs.filter.fasta 100
-cat $outdir/ass/contigs.filter.fasta >>$seg_ref
+if [ -f "$outdir/ass/contigs.filter.fasta" ]; then
+  cat $outdir/ass/contigs.filter.fasta >>$seg_ref
+fi
 
 end=$(date +%s)
 take=$(( end - start ))
-echo two Time taken to map reads is ${take} seconds. # >> ${sample}.log
-
-
+echo two Time taken to unmap-reads assembly is ${take} seconds. # >> ${sample}.log
 
 
 bwa index $seg_ref
@@ -69,17 +72,16 @@ samtools index $sample.seg.bam
 samtools depth -aa $sample.seg.bam >$sample.seg.bam.depth
 rm $sample.seg.unsort.bam
 
-# !
+
 echo graph-building...
 python3 $dir/bam2graph.py $sample.seg.bam $sample.graph.txt $sample.seg.bam.depth
-# :<<!
 python3 $dir/plot_graph.py $sample.graph.txt $sample.plot.graph.pdf
 echo "matching is done..."
 /home/wangshuai/softwares/seqGraph/build/matching -b --model 1 -v 1 -g $sample.graph.txt -r $sample.solve.path.txt -c $sample.solve.c.path.txt -m $sample.new.graph.txt --break_c
 
 end=$(date +%s)
 take=$(( end - start ))
-echo three Time taken to map reads is ${take} seconds. # >> ${sample}.log
+echo three Time taken to matching is ${take} seconds. # >> ${sample}.log
 
 
 python3 $dir/graph2contig.py $seg_ref $sample.solve.path.txt $sample.contigs.fasta
@@ -89,41 +91,17 @@ bwa mem -t $threads -R "@RG\tID:id\tSM:sample\tLB:lib" $sample.contigs.fasta $fq
   | samtools view -bhS -> $sample.contigs.unsort.bam
 samtools sort -o $sample.contigs.bam $sample.contigs.unsort.bam
 samtools index $sample.contigs.bam
-!
 end=$(date +%s)
 take=$(( end - start ))
-echo four Time taken to map reads is ${take} seconds. # >> ${sample}.log
-freebayes -f $sample.contigs.fasta -p 1 $sample.contigs.bam >$sample.contigs.vcf
-# delly call -g $sample.contigs.fasta -o $sample.new.sv.bcf $sample.contigs.bam
-# bcftools view $sample.new.sv.bcf >$sample.new.sv.vcf
+echo four Time taken to alignment to contigs is ${take} seconds. # >> ${sample}.log
+
+
+### freebayes-parallele requires the package: parallele and vcflib, can be installed by conda, 
+### remember to edit the conda_env/bin/vcffirstheader with python2
+freebayes-parallel <(python $dir/fasta_generate_regions.py $sample.contigs.fasta.fai 100000) $threads -p 1 -f $sample.contigs.fasta $sample.contigs.bam >$sample.contigs.vcf
+# freebayes -f $sample.contigs.fasta -p 1 $sample.contigs.bam >$sample.contigs.vcf
 svaba run -t $sample.contigs.bam -G $sample.contigs.fasta -a $sample.svaba_2_ -p $threads
 python $dir/polish_contigs.py $sample.contigs.vcf $sample.svaba_2_.svaba.indel.vcf $sample.svaba_2_.svaba.sv.vcf $sample.contigs.fasta $sample.contigs.final.fasta
 end=$(date +%s)
 take=$(( end - start ))
-echo five Time taken to map reads is ${take} seconds. # >> ${sample}.log
-# vcftools --vcf $sample.contigs.vcf --minQ 20 --recode --recode-INFO-all --out $sample.contigs_q20
-# bgzip -f $sample.contigs_q20.recode.vcf
-# tabix -f $sample.contigs_q20.recode.vcf.gz
-# cat $sample.contigs.fasta |bcftools consensus -H 1 $sample.contigs_q20.recode.vcf.gz >$sample.contigs.consensus.fasta
-
-
-
-# bwa index $sample.contigs.consensus.fasta
-# samtools faidx $sample.contigs.consensus.fasta
-# bwa mem -t $threads -R "@RG\tID:id\tSM:sample\tLB:lib" $sample.contigs.consensus.fasta $fq1 $fq2 \
-#   | samtools view -bhS -> $sample.contigs.new.unsort.bam
-# samtools sort -o $sample.contigs.new.bam $sample.contigs.new.unsort.bam
-# samtools index $sample.contigs.new.bam
-# svaba run -t $sample.contigs.new.bam -G $sample.contigs.consensus.fasta -a $sample.svaba_new_
-# python $dir/split_contigs.py $sample.svaba_new_.svaba.sv.vcf $sample.contigs.consensus.fasta $sample.contigs.consensus.split.fasta
-
-
-
-end=$(date +%s)
-take=$(( end - start ))
-echo six Time taken to map reads is ${take} seconds. # >> ${sample}.log
-
-
-# blastn -subject $seg_ref -query $seg_ref -out $sample.blast.txt -outfmt 6 #remove the overlap between contig and ref-segments
-# python $dir/delete_repeat.py $seg_ref $seg_ref.new $sample.blast.txt
-# mv $seg_ref.new $seg_ref
+echo five Time taken to variants is ${take} seconds. # >> ${sample}.log
